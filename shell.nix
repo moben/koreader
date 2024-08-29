a35a1cac9f36ecd2c8081f8edceabffea0181780
Benedikt McMullin <benedikt@mcmullin.de>
2024-08-29 12:13:35 +0200

    wip nix? not sure what I was doing...

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
   nixpkgsSource ? (builtins.fetchTarball {
     name = "nixpkgs";
     url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
-    sha256 = "sha256:1xbvd82w5ry8bl0df48algb35wj4l2p1lmpaaadp0da1arvnn61f";
+    sha256 = "sha256:1kkzvi3vc14k6sp829hc73228yixjl4m4sp37scjynx4shmhbpb1";
   }),
   pkgs ? import nixpkgsSource {
     config.allowUnfree = true;
@@ -16,27 +16,27 @@ let
   buildToolsVersion = "30.0.2";
   # Use cmakeVersion when you define androidComposition
   # androidComposition = pkgs.androidenv.androidPkgs_9_0;
-  androidComposition = pkgs.androidenv.composeAndroidPackages {
-    cmdLineToolsVersion = "8.0";
-    toolsVersion = "26.1.1";
-    platformToolsVersion = "34.0.1";
-    buildToolsVersions = [ buildToolsVersion ];
-    # includeEmulator = true;
-    emulatorVersion = "33.1.6";
-    platformVersions = [ "30" ];
-    includeSources = false;
-    includeSystemImages = false;
-    systemImageTypes = [ "google_apis_playstore" ];
-    abiVersions = [ "arm64-v8a" "armeabi-v7a" ];
-    # cmakeVersions = [ "3.10.2" ];
-    includeNDK = true;
-    ndkVersions = ["22.0.7026061"];
-    useGoogleAPIs = false;
-    useGoogleTVAddOns = false;
-    includeExtras = [
-      # "extras;google;gcm"
-    ];
-  };
+  # androidComposition = pkgs.androidenv.composeAndroidPackages {
+  #   cmdLineToolsVersion = "8.0";
+  #   toolsVersion = "26.1.1";
+  #   platformToolsVersion = "34.0.1";
+  #   buildToolsVersions = [ buildToolsVersion ];
+  #   # includeEmulator = true;
+  #   emulatorVersion = "33.1.6";
+  #   platformVersions = [ "30" ];
+  #   includeSources = false;
+  #   includeSystemImages = false;
+  #   systemImageTypes = [ "google_apis_playstore" ];
+  #   abiVersions = [ "arm64-v8a" "armeabi-v7a" ];
+  #   # cmakeVersions = [ "3.10.2" ];
+  #   includeNDK = true;
+  #   ndkVersions = ["22.0.7026061"];
+  #   useGoogleAPIs = false;
+  #   useGoogleTVAddOns = false;
+  #   includeExtras = [
+  #     # "extras;google;gcm"
+  #   ];
+  # };
 
 in
 pkgs.mkShell rec {
@@ -45,7 +45,7 @@ pkgs.mkShell rec {
     libpng
     libxkbcommon
     mesa
-    openjdk11
+    # openjdk11
     SDL2
     vulkan-headers
     vulkan-loader
@@ -59,6 +59,7 @@ pkgs.mkShell rec {
   nativeBuildInputs = with pkgs; [
     autoconf
     automake
+    gettext
     bash
     ccache
     chrpath
@@ -76,15 +77,15 @@ pkgs.mkShell rec {
     pkg-config
     python3
   ];
-  ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
-  ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
+  # ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+  # ANDROID_NDK_ROOT = "${ANDROID_SDK_ROOT}/ndk-bundle";
 
-  GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
+  # GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
   CC_FOR_BUILD="gcc";
 
   # Use the same cmakeVersion here
   shellHook = ''
-    export PATH="$(echo "$ANDROID_SDK_ROOT/cmake/${cmakeVersion}".*/bin):$PATH"
+    # export PATH="$(echo "$ANDROID_SDK_ROOT/cmake/${cmakeVersion}".*/bin):$PATH"
     export CC_FOR_BUILD=gcc
   '';
 }
